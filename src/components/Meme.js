0ae558c1724388ae036cb9a9806cf8f1cbf4f5cc
vigneshuthra <vigneshuthra@gmail.com>
2023-07-29 20:07:00 +0100

    added mock data and image layout

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,45 @@
 import React from "react";
+import memesData from "../memesData";
 
+export default function Meme() {
+  const [meme, setMeme] = React.useState({
+     topText:"",
+     bottomText:"",
+     randomImage: "http://i.imgflip.com/1bij.jpg"
+  });
 
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
-export default function Meme(){
+  let url
+  function getMemeImage() {
+    const memeArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    url = memeArray[randomNumber].url;
+
+    setMeme( prevState => ({
+      ...prevState,
+      randomImage: url
+    }));
+  }
 
   return (
     <main>
-      <form className="form">
-        <input className="form--input" type="text" placeholder="Top text"></input>
-        <input className="form--input" type="text" placeholder="Bottom text"></input>
-        <button className="form--button">Get a new meme template</button>
-      </form>
+      <div className="form">
+        <input
+          className="form--input"
+          type="text"
+          placeholder="Top text"
+        ></input>
+        <input
+          className="form--input"
+          type="text"
+          placeholder="Bottom text"
+        ></input>
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme template
+        </button>
+      </div>
+      <img className="meme--image" src={meme.randomImage} />
     </main>
-  )
+  );
 }
